777e0c6946448fa14a6a24d53dd0e764b54455dd
zhangwei <zhangwei@cetiti.com>
2018-05-30 16:09:21 +0800

    Initial commit

---
+++ vz-learn-distributed/src/main/java/com/victorzhang/learn/distributed/JavaSerialize.java
@@ -0,0 +1,41 @@
+package com.victorzhang.learn.distributed;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+/**
+ * RPC 之 Java 内置对象序列化
+ *
+ * @author zhangwei
+ * @email zhangwei@cetiti.com
+ * @date 2018-05-30 15:55:27
+ */
+public class JavaSerialize {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        final Person person = new Person("zhangwei", "123456");
+
+        JavaSerialize serializeUtil = new JavaSerialize();
+        byte[] personBytes = serializeUtil.serialize(person);
+        System.out.println(serializeUtil.deserialize(personBytes));
+    }
+
+    public byte[] serialize(Object object) throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(baos);
+        oos.writeObject(object);
+
+        return baos.toByteArray();
+    }
+
+    public Object deserialize(byte[] bytes) throws IOException, ClassNotFoundException {
+        ByteArrayInputStream bios = new ByteArrayInputStream(bytes);
+        ObjectInputStream ois = new ObjectInputStream(bios);
+
+        return ois.readObject();
+    }
+
+}
---
+++ vz-learn-distributed/src/main/java/com/victorzhang/learn/distributed/Person.java
@@ -0,0 +1,46 @@
+package com.victorzhang.learn.distributed;
+
+import java.io.Serializable;
+
+/**
+ * 人物对象,进行序列化必须实现Serializable接口，否则报java.io.NotSerializableException
+ * @author zhangwei
+ * @email zhangwei@cetiti.com
+ * @date 2018-05-30 15:57:08
+ */
+public class Person implements Serializable {
+
+    private String username;
+    private String password;
+
+    public Person() {}
+
+    public Person(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
